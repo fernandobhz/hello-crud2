0e6682e9b37bb32797de1ceebe78395e44e35605
Fernando Reis Guimarães <fernandobhz@gmail.com>
2021-05-27 21:45:22 -0300

    final

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ app.use(express.json());
 const port = 3000;
 
 const usuarioFernando = {
-    id: 1,
+    id: 0,
     nome: 'fernando reis guimaraes!',
     idade: 35
 };
@@ -14,6 +14,41 @@ const usuarios = [usuarioFernando];
 
 app.get('/usuarios', (req, res) => {
     res.json(usuarios);
+});
+
+app.post('/usuarios', (req, res) => {
+    const { body: { nome, idade } } = req;
+    const id = usuarios.length;
+
+    const novoUsuario = {
+        id,
+        nome,
+        idade
+    };
+
+    usuarios.push(novoUsuario);
+    res.status(201).json(novoUsuario);
+});
+
+app.put('/usuarios/:id', (req, res) => {
+    const { id } = req.params;
+    const { nome, idade } = req.body;
+
+    const usuario = usuarios.find(usuario => usuario.id === Number(id));
+    usuario.nome = nome;
+    usuario.idade = idade;
+
+    res.json(usuario);
+});
+
+app.delete('/usuarios/:id', (req, res) => {
+    const { id } = req.params;
+
+    const index = usuarios.findIndex(usuario => usuario.id === Number(id));
+    
+    usuarios.splice(index, 1);
+
+    res.status(204).send();
 });
 
 app.listen(port, () => {
